2df03bf01d7d06b8e1529ca1d4e070ca9bb267ac
Maxime Moreillon <m.moreillon@gmail.com>
2025-11-23 10:41:28 +0900

    initial commit

---
+++ import/main.py
@@ -0,0 +1,35 @@
+import requests
+from dotenv import load_dotenv
+from os import getenv, path
+
+load_dotenv()
+
+ACCESS_TOKEN = getenv("ACCESS_TOKEN")
+BASE_URL = getenv("BASE_URL")
+
+headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
+
+images_directory = "../images"
+
+
+def get_image_list():
+    res = requests.get(f"{BASE_URL}/images?limit=3", headers=headers)
+    data = res.json()
+    return data["items"]
+
+
+def save_image(image):
+    image_id = image["_id"]
+    image_url = f"{BASE_URL}/images/{image_id}"
+    image_path = path.join(images_directory, image_id)
+    res = requests.get(image_url, stream=True)
+    res.raise_for_status()
+    with open(image_path, "wb") as file:
+        for chunk in res.iter_content(chunk_size=8192):
+            file.write(chunk)
+    print(image_id)
+
+
+images = get_image_list()
+for image in images:
+    save_image(image)
